1978116b479926f2496402cf8b4269073536c3f5
Shawn <codeshawnachu@gmail.com>
2019-01-13 17:18:37 -0800

    Add main.lua file

---
+++ main.lua
@@ -0,0 +1,8 @@
+function love.draw()
+    love.graphics.print("Hello World", 0, 0);
+end
+
+function love.load()
+    love.window.setTitle("Superior Battle")
+    love.window.setMode(1280, 720)
+end
